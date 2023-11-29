b75fc049096069f0190a4a935874d47378c28948
IKalonji <IKalonji@student.wethinkcode.co.za>
2023-11-29 22:26:15 +0200

    Fixed bundle size

--- src/app/models/market-assets.ts
+++ src/app/models/market-assets.ts
@@ -4,7 +4,19 @@ export const MarketAssets: MarketAsset[] = [
     {
         brand: "DeAIM Denim Company",
         slogan: "Wear your Truth",
-        image: "../assets/deaim_demin.png",
+        image: "../../assets/deaim_denim.png",
+        details: "Use this asset for a one time payment of $500 USD on game launch"
+    },
+    {
+        brand: "Fruzz Soda",
+        slogan: "Quence your THIRST!",
+        image: "../../assets/fruzz_soda.png",
+        details: "Use this asset for a one time payment of $500 USD on game launch"
+    },
+    {
+        brand: "'Beest Footwear'",
+        slogan: "Let your feet dance",
+        image: "../../assets/beest footwear.png",
         details: "Use this asset for a one time payment of $500 USD on game launch"
     }
 ]
